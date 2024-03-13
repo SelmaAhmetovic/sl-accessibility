3d0b20b7932c973c95f6c636d063a3139fca5e51
Selma Repuh <selma.repuh@endava.com>
2024-03-13 13:30:12 +0100

    refactor: moved sidebar and toolbar to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,51 @@ import { MainComponent } from './core/main/main.component';
 import { ToolbarComponent } from './core/toolbar/toolbar.component';
 
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { Subject } from 'rxjs';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { SideContentComponent } from './core/side-content/side-content.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    SidebarComponent, 
-    MainComponent,
+    //SidebarComponent, 
+    //MainComponent,
     CommonModule,
-    ToolbarComponent, 
+    //ToolbarComponent, 
+    RouterOutlet,
+    MatSidenavModule,
+    MatSidenav,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    SideContentComponent,
+    MatToolbarModule,
+
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'material-responsive-sidenav';
-  isMobile= true;
+  //isMobile= true;
   changingValue: Subject<boolean> = new Subject();
 
-  constructor(private observer: BreakpointObserver) {}
+  @Input() changing!: Subject<boolean>;
+  @Input() isMobile = false;
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  isCollapsed = true;
+
+  constructor(private observer: BreakpointObserver,
+              private router: Router) {}
 
   ngOnInit() {
     this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
@@ -37,8 +59,38 @@ export class AppComponent implements OnInit {
     });
   }
 
-  toggleMenu() {
+  /*toggleMenu() {
     this.changingValue.next(true);
+  }*/
+
+  toggleMenu() {
+    if(this.isMobile){
+      this.sidenav.toggle();
+      this.isCollapsed = false; // On mobile, the menu can never be collapsed
+    } else {
+      this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+      this.isCollapsed = !this.isCollapsed;
+    }
+
+}
+
+navigate(route: string) {
+  this.router.navigate([route]);
+}
+
+
+checkKeyboardPress(event: KeyboardEvent, route?: string){
+  if(event.key === "Enter" && route) {
+    this.navigate(route)
+    return;
   }
+  if(event.key === "Enter" && !route) {
+    this.toggleMenu()
+    return;
+  }
+  if(event.key === "Tab") {
+    return;
+  }
+}
 
 }
